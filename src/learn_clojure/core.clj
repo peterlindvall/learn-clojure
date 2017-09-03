8abbdd315c4e9267426cb1d84aade3d7bfd369b4
Peter Lindvall <peter.lindvall@gmail.com>
2017-09-03 23:35:44 +0200

    Started using atom for state

--- src/learn_clojure/core.clj
+++ src/learn_clojure/core.clj
@@ -17,23 +17,67 @@
    [3 3 3 4 5 6 7 8 9]                                      ;r2 c2
    ])
 
-(def mBoard (createBoard))
+(defn createEmptyBoard []
+  [[0 0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0 0]
+   [0 0 0 0 0 0 0 0 0]])
+(createEmptyBoard)
+
+(defn createMediumBoard []
+  [[0 0 5 0 0 0 9 0 0]
+   [0 9 6 0 0 0 0 0 0]
+   [0 8 0 0 0 7 4 0 6]
+   [0 8 4 1 0 0 6 0 0]
+   [2 0 0 0 0 7 0 0 0]
+   [0 0 0 5 4 0 3 0 0]
+   [0 0 0 0 0 6 5 7 0]
+   [0 0 9 0 3 5 0 0 0]
+   [0 0 0 0 7 0 0 2 4]])
+
+
+(defn createInitialState []
+  (createMediumBoard))
+
+;defonce locks. Not good for debug purposes during development
+(def state-atom (atom {:board (createInitialState)
+                       :initialized? false}))
+
+(defn clearBoard []
+  (swap! state-atom assoc :board (createEmptyBoard)))
+
+(swap! state-atom assoc :initialized? false)
+(get-in @state-atom [:initialized?])
+(swap! state-atom assoc :initialized? true)
+(get-in @state-atom [:initialized?])
+
+(get-in @state-atom [:board])
+(clearBoard)
+(get-in @state-atom [:board])
+
+(defn undoMove []
+  ;ToDo Add listener to state changes and save all boards
+  )
+
 (def notSet 0)
 
 (defn getBoard []
-  mBoard
+  (get-in @state-atom [:board])
   )
+(getBoard)
 
 (defn getCell [r, c, board]
   "Returns a vector with all digits in cell. 0-indexed"
-  ;ToDo Make grid size independent
-  (assert (and (> r -1) (> c -1)))
-  (assert (and (< r 3) (< c 3)))
   (nth board (+ (* r 3) c))
   )
 
-(defn getValue [r c pos]
-  (nth (nth (getBoard) r) c)
+(defn getValue [r c]
+  (nth (nth (@state-atom :board) r) c)
   )
 
 (defn isValidCellVerbose? [cell]
@@ -48,7 +92,7 @@
     (println tooSmall " too small values"))
   )
 
-(defn isCellComplete? [cell gridSize]
+(defn isCellComplete? [cell]
   "does cell contain gridSize unique values larger than 0 and not larger than gridsize"
   (if (= gridSize (count cell))
     (if (= gridSize (count (set cell)))
@@ -71,7 +115,10 @@
 
 (defn getRow [rix board]
   "Returns the row at rownum. Zero indexed"
-  (let [[cell1 cell2 cell3] [(getCell rix 0 board) (getCell rix 1 board) (getCell rix 2 board)]]
+  ;ToDo - broken. Write test!
+  (let [cell1 (getCell rix 0 board) ;mod 3
+        cell2 (getCell rix 1 board)
+        cell3 (getCell rix 2 board)]
     (vector (nth cell1 0)(nth cell1 1)(nth cell1 2)
             (nth cell2 0)(nth cell1 2)(nth cell2 2)
             (nth cell3 0)(nth cell3 1)(nth cell3 2))))
@@ -101,13 +148,43 @@
   "Fill cell with random values"
   ;vector with all digs
   (let [store [1 2 3 4 5 6 7 8 9]
-        cell []
-        dig (rand-int (count store ))]
-    (println (conj (conj cell dig) (rand-int (count store ))))
+        lcell []
+        dig (rand-int (count store))]
+    (loop [x 8]
+      (when (>= x 0)
+        (conj lcell (nth store (rand-int (count store))))
+        ;(println (conj cell (nth store (rand-int (count store )))))
+        (println lcell)
 
-      ;(println cell)
+        ))
+    lcell
     ))
-(createCompleteCell)
+
+(def cell (createCompleteCell))
+(println cell)
+
+
+(defn printBoard []
+  (println (getRow 0 (@state-atom :board)))
+  (println (getRow 1 (@state-atom :board)))
+  (println (getRow 2 (@state-atom :board)))
+  (println (getRow 3 (@state-atom :board)))
+  (println (getRow 4 (@state-atom :board)))
+  (println (getRow 5 (@state-atom :board)))
+  (println (getRow 6 (@state-atom :board)))
+  (println (getRow 7 (@state-atom :board)))
+  (println (getRow 8 (@state-atom :board)))
+  )
+(get-in @state-atom [:board])
+(printBoard)
+
+
+
+
+    (loop [x 10]
+  (when (> x 1)
+    (println x)
+    (recur (- x 2))))
 
 
 (macroexpand `#(< 1 (freqs %)))
@@ -117,11 +194,11 @@
 (def cellSet (set (getCell 0 2 mBoard)))
 (count cellSet)
 (count (frequencies (getCell 0 2 mBoard)))
-(isCellComplete? (getCell 0 2 mBoard) gridSize)
+(isCellComplete? (getCell 0 2 mBoard))
 (getCell 2 0 mBoard)
-(isCellComplete? (getCell 2 0 mBoard) gridSize)
-(isCellComplete? (getCell 2 0 mBoard) gridSize)
-(isCellComplete? (getCell 2 0 mBoard) gridSize)
+(isCellComplete? (getCell 2 0 mBoard))
+(isCellComplete? (getCell 2 0 mBoard))
+(isCellComplete? (getCell 2 0 mBoard))
 (set (getCell 2 0 mBoard))
 
 
--- test/learn_clojure/core_test.clj
+++ test/learn_clojure/core_test.clj
@@ -9,16 +9,17 @@
               [1 2 0 4 5 6 7 8 9] ;r0 c2 (invalid value, to small)
               [1 2 3 4 5 6 7 8 10] ;r1 c0 (invalid value, to large)
               [2 2 3 4 5 6 7 8 9] ;r1 c1 (has duplicate value)
-              [2 3 3 4 5 6 7 8 9] ;r1 c2
-              [3 1 2 4 5 6 7 8 9] ;r2 c0
+              [1 2 3 4 5 6 7 8 9 10] ;r1 c2 (too many values)
+              [1 2 3 4 5 6 7 8 9] ;r2 c0
               [3 2 3 4 5 6 7 8 9] ;r2 c1
               [3 3 3 4 5 6 7 8 9]]] ;r2 c2
 
-    (is (isCellComplete? (nth board 0) 9))
-    (is (not (isCellComplete? (nth board 1) 9)))
-    (is (not (isCellComplete? (nth board 2) 9)))
-    (is (not (isCellComplete? (nth board 3) 9)))
-    (is (not (isCellComplete? (nth board 4) 9)))))
+    (is (isCellComplete? (nth board 0)))
+    (is (not (isCellComplete? (nth board 1))))
+    (is (not (isCellComplete? (nth board 2))))
+    (is (not (isCellComplete? (nth board 3))))
+    (is (not (isCellComplete? (nth board 4))))
+    (is (not (isCellComplete? (nth board 5))))))
 
 (deftest getCell-test
   (let [board [[1 2 3 4 5 6 7 8 9] ;r0 c0
@@ -31,4 +32,20 @@
     (is (= (getCell 0 0 board) [1 2 3 4 5 6 7 8 9]))
     (is (= (getCell 1 1 board) [2 2 3 4 5 6 7 8 9]))
     (is (= (getCell 1 2 board) [3 3 3 4 5 6 7 8 9]))
-  ))
+    ))
+
+(deftest getCell-test
+  (let [cell00 [1 2 3 4 5 6 7 8 9]
+        cell11 [2 2 3 4 5 6 7 8 9]
+        cell12 [3 3 3 4 5 6 7 8 9]
+        board [cell00                          ;r0 c0
+               [1 2 3 4 5 6 7 8]                            ;r0 c1
+               [1 2 0 4 5 6 7 8 9]                          ;r0 c2
+               [1 2 3 4 5 6 7 8 10]                         ;r1 c0
+               cell11                          ;r1 c1
+               cell12]]                        ;r1 c2
+
+    (is (= (getCell 0 0 board) cell00))
+    (is (= (getCell 1 1 board) cell11))
+    (is (= (getCell 1 2 board) cell12))
+    ))
